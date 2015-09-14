653e7a99ad836a18f68b27535ab9db6d7a571a0c
Wilczogon <wilczogon@gmail.com>
2015-09-14 22:58:49 +0200

    converted to maven project, trying to convert to jpa project - soon (Sadam, please install m2e plugin to your Eclipse)

--- bookstore/src/pl/edu/agh/student/sadam/bookstore/Book.java
+++ bookstore/src/pl/edu/agh/student/sadam/bookstore/Book.java
@@ -1,5 +1,10 @@
 package pl.edu.agh.student.sadam.bookstore;
 
+@Entity
 public class Book {
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long isbn;
+	private String title;
+	private String author;
 }
