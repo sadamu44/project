2bab052ebdc8e8357c7e32b27bdc5dbd5df1106a
Wilczogon <wilczogon@gmail.com>
2015-09-15 22:24:30 +0200

    Example is working :D Now, it is time to do main part

--- bookstore/src/pl/edu/agh/student/sadam/bookstore/Book.java
+++ bookstore/src/pl/edu/agh/student/sadam/bookstore/Book.java
@@ -1,10 +1,38 @@
 package pl.edu.agh.student.sadam.bookstore;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @Entity
 public class Book {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long isbn;
 	private String title;
 	private String author;
+	
+	public Book(){
+		//TODO ???
+	}
+	
+	public Book(long isbn, String title, String author){
+		this.isbn = isbn;
+		this.setTitle(title);
+		this.setAuthor(author);
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
 }
--- bookstore/src/pl/edu/agh/student/sadam/bookstore/BookstoreManager.java
+++ bookstore/src/pl/edu/agh/student/sadam/bookstore/BookstoreManager.java
@@ -4,10 +4,24 @@ import java.security.InvalidParameterException;
 import java.util.List;
 import java.util.Map;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import pl.edu.agh.student.sadam.bookstore.Book;
+import pl.edu.agh.student.sadam.bookstore.NotEnoughBooksInBookstoreException;
+import pl.edu.agh.student.sadam.bookstore.NotExistsOnPossibleBooksListException;
+
 public class BookstoreManager {
-	private List<Book> possibleBooks;
-	private Map<Book, Integer> warehouse;
-	private Map<Book, Integer> bookstore;
+	private static EntityManagerFactory factory;
+	//private List<Book> possibleBooks;
+	//private Map<Book, Integer> warehouse;
+	//private Map<Book, Integer> bookstore;
+	
+	public static void main(String[] args){
+		BookstoreManager manager = new BookstoreManager();
+	}
 	
 	public BookstoreManager(){
 		//konstruktor
@@ -15,11 +29,29 @@ public class BookstoreManager {
 	}
 	
 	private void init(){
-		//tutaj b�dziemy �adowa� dane z bazy danych
+		factory = Persistence.createEntityManagerFactory("bookstore");
+	    EntityManager em = factory.createEntityManager();
+	    
+	    Query q = em.createQuery("select b from Book b");
+	    List<Book> bookList = q.getResultList();
+	    for (Book todo : bookList) {
+	      System.out.println(todo);
+	    }
+	    System.out.println("Size: " + bookList.size());
+
+	    // create new todo
+	    em.getTransaction().begin();
+	    Book todo = new Book(1, "Robinson Crusoe", "Dafoe..?");
+	    //book.setSummary("This is a test");
+	    //book.setDescription("This is a test");
+	    em.persist(todo);
+	    em.getTransaction().commit();
+
+	    em.close();
 	}
 	
 	public void addPossibleBook(Book book){
-		possibleBooks.add(book);
+		//possibleBooks.add(book);
 	}
 	
 	public void moveBookToBookstore(Book book, int quantity) throws NotExistsOnPossibleBooksListException, NotEnoughBooksInBookstoreException{
@@ -27,22 +59,22 @@ public class BookstoreManager {
 		if(quantity < 0)
 			throw new InvalidParameterException();
 		
-		if(!warehouse.containsKey(book))
+		/*if(!warehouse.containsKey(book))
 			throw new NotExistsOnPossibleBooksListException();
 		
 		if(warehouse.get(book) < quantity)
 			throw new NotEnoughBooksInBookstoreException();
 		
 		warehouse.put(book, warehouse.get(book) - quantity);
-		bookstore.put(book, bookstore.get(book) + quantity);
+		bookstore.put(book, bookstore.get(book) + quantity);*/
 	}
 	
 	public void addBookToWarehouse(Book book, int quantity) throws NotExistsOnPossibleBooksListException{
 		if(quantity < 0)
 			throw new InvalidParameterException();
 		
-		if(!warehouse.containsKey(book))
-			throw new NotExistsOnPossibleBooksListException();
+		/*if(!warehouse.containsKey(book))
+			throw new NotExistsOnPossibleBooksListException();*/
 	}
 	
 }
